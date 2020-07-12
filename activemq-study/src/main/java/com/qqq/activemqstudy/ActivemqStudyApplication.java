96f9795db93fa325aadd2beec784e8eb96b7cdf8
qqqcode <1278323354@qq.com>
2020-07-12 22:59:16 +0800

    创建项目

---
+++ activemq-study/src/main/java/com/qqq/activemqstudy/ActivemqStudyApplication.java
@@ -0,0 +1,13 @@
+package com.qqq.activemqstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqStudyApplication.class, args);
+    }
+
+}
